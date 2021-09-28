e769e7e0c6c433d9564358f4ff8deba029a2fedc
mahimanzum <mahimanzum@gmail.com>
2021-09-28 12:49:49 -0400

    it's done but i need to make sure if we are strictly following the grammar. My dsl generator will also take in but at min and sell at max commands

--- stock_dsl.py
+++ stock_dsl.py
@@ -8,19 +8,43 @@ def read_json(path):
 data = read_json("stocks.json")
 print(data.keys())
 #INSERT INTO BuyRequests (NumShares, Symbol, MaxPrice, AccountID) VALUES (‘100’, ‘IBM’, ‘45’,  ‘Hokie123’)
-sql_file = []
-if 'buy' in data.keys():
-    for item in data['buy']:
-        if 'at max' in item.keys():
-            sql_file.append("INSERT INTO BuyRequests (NumShares, Symbol, MaxPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at max'], data['user id']))
-        elif 'at min' in item.keys():
-            sql_file.append("INSERT INTO BuyRequests (NumShares, Symbol, MinPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at min'], data['user id']))
+def make_sql():
+    sql_file = []
+    if 'buy' in data.keys():
+        for item in data['buy']:
+            if 'at max' in item.keys():
+                sql_file.append("INSERT INTO BuyRequests (NumShares, Symbol, MaxPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at max'], data['user id']))
+            elif 'at min' in item.keys():
+                sql_file.append("INSERT INTO BuyRequests (NumShares, Symbol, MinPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at min'], data['user id']))
+    if 'sell' in data.keys():
+        for item in data['sell']:
+            if 'at max' in item.keys():
+                sql_file.append("INSERT INTO SellRequests (NumShares, Symbol, MaxPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at max'], data['user id']))
+            elif 'at min' in item.keys():
+                sql_file.append("INSERT INTO SellRequests (NumShares, Symbol, MinPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at min'], data['user id']))
+    return sql_file
 
-if 'sell' in data.keys():
-    for item in data['sell']:
-        if 'at max' in item.keys():
-            sql_file.append("INSERT INTO SellRequests (NumShares, Symbol, MaxPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at max'], data['user id']))
-        elif 'at min' in item.keys():
-            sql_file.append("INSERT INTO SellRequests (NumShares, Symbol, MinPrice, AccountID) VALUES ({}, {},{}, {})".format(item['shares'], item['stock symbol'], item['at min'], data['user id']))
+def make_dsl():
+    trades = []
+    if 'buy' in data.keys():
+        for item in data['buy']:
+            if 'at max' in item.keys():
+                trade = "{} {} shares buy at max {}".format(item['shares'],item['stock symbol'], item['at max'])
+                trades.append(trade)
+            elif 'at min' in item.keys():
+                trade = "{} {} shares buy at min {}".format(item['shares'],item['stock symbol'], item['at min'])
+                trades.append(trade)
+    if 'sell' in data.keys():
+        for item in data['sell']:
+            if 'at max' in item.keys():
+                trade = "{} {} shares sell at max {}".format(item['shares'],item['stock symbol'], item['at max'])
+                trades.append(trade)
+            elif 'at min' in item.keys():
+                trade = "{} {} shares sell at min {}".format(item['shares'],item['stock symbol'], item['at min'])
+                trades.append(trade)
+    
+    stock_trade_requests = "("+ ",".join(trades)+")" +" for account {}".format(data['user id'])
+    print(stock_trade_requests)
+make_dsl()
+    
 
-pprint(sql_file)
